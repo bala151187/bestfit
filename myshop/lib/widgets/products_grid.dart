1cf275a66fc4c29f54fe919e811cce464325d177
Janakiraman, Balamurugan <balamurugan.janakiraman@cengage.com>
2020-02-12 08:31:18 -0500

    Adding myshop_app

---
+++ myshop/lib/widgets/products_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+  ProductsGrid(this.showFavs);
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<ProductsProvider>(context);
+    final products = showFavs ? productData.favoriteItems : productData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        //create: (ctx) => products[index],
+        value: products[index],
+        child: ProductItem(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
